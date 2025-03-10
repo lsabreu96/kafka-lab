9f6b96eead20a33ff9efc639665af9e2835908f8
Lucas Abreu <sa.lucas96@gmail.com>
2025-03-10 08:13:44 -0300

    ajuste(gerador): adiciona app teste e seu compose

---
+++ gerador_de_mensagens/src/message_generator.py
@@ -0,0 +1,33 @@
+import time
+from faker import Faker
+from datetime import datetime
+import random
+from confluent_kafka import Producer
+import os
+
+
+# Inicializar o Faker
+fake = Faker()
+produtor = Producer({'bootstrap.servers': os.getenv('KAFKA_BOOTSTRAP_SERVER')})
+
+usuarios = [fake.name() for _ in range(3)]
+
+def gerar_saida():
+    while True:
+        hora_atual = datetime.now().strftime("%H:%M:%S")
+
+        usuario  = random.choice(usuarios)
+        
+        valor = round(random.uniform(1.0, 100.0), 2)
+
+        tipo = random.choice(['C', 'D'])
+        
+        produtor.produce(
+            os.getenv('KAFKA_TOPIC'),
+            f"{hora_atual}:{usuario}:{valor}:{tipo}".encode('utf-8')
+        )
+        
+        time.sleep(1)
+
+if __name__ == "__main__":
+    gerar_saida()

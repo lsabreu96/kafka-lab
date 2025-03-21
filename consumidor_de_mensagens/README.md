36c226c154a41625746a4d811fe67e45295cfb23
lsabreu <sa.lucas96@gmail.com>
2025-03-21 18:16:55 -0300

    melhoria(consumidor): implementa logicazinha de clientes

---
+++ consumidor_de_mensagens/README.md
@@ -0,0 +1,7 @@
+# Consumidor de mensagens
+
+Exemplo simples para testar consumo de mensagens usando a lib da Confluent pro Kafka.
+
+A aplicação consome mensagens de um tópico chamado transações que vem no formato
+horario:cliente:valor:tipo de operação
+
--- consumidor_de_mensagens/src/message_consumer.py
+++ consumidor_de_mensagens/src/message_consumer.py
@@ -3,11 +3,12 @@ from confluent_kafka import Consumer
 import os
 import logging
 
+
 logging.basicConfig(level=logging.INFO)
 consumer_config = {
     'bootstrap.servers': os.getenv('KAFKA_BOOTSTRAP_SERVER'),
     'group.id': os.getenv('KAFKA_CONSUMER_GROUP_ID'),
-    'auto.offset.reset': 'earliest'
+    'auto.offset.reset': os.getenv('KAFKA_OFFSET_RESET')
 }
 
 topico = os.getenv('KAFKA_CONSUMER_TOPICS')
@@ -17,10 +18,46 @@ consumidor.subscribe([topico])
 
 logging.debug(f"Consumidor iniciado para o tópico: {topico}")
 
-def consome_mensagens():
-    while True:
-        msg = consumidor.poll(1.0)  # Timeout ajustado
+clientes = []
 
+class Cliente:
+    _instancias = []
+
+    def __init__(self, nome:str, saldo:float=0):
+        self.nome = nome
+        self.debitos = []
+        self.creditos = []
+        Cliente._instancias.append(self)
+
+
+    @classmethod
+    def cria_ou_recupera_cliente(cls, nome:str):
+        for instancia in cls._instancias:
+            if instancia.nome == nome:
+                return instancia
+ 
+        logging.info(f"Criando usuario {nome}")        
+        return cls(nome)
+
+
+    def processa_entrada(self, valor: float, operacao: str):
+        if operacao == 'D':
+            self.debitos.append(float(valor))
+        
+        if operacao == 'C':
+            self.creditos.append(float(valor))
+
+        logging.info(f"Saldo atual do usuário é de {self.processa_saldo()}")
+
+
+    def processa_saldo(self)->float:
+        return sum(self.creditos) + sum(self.debitos)
+
+
+def processa_clientes():
+    while True:
+        msg = consumidor.poll(1.0)
+    
         if msg is None:
             logging.debug("Nenhuma mensagem recebida...")
             continue
@@ -28,9 +65,15 @@ def consome_mensagens():
         if msg.error():
             logging.error(f"Erro no consumo: {msg.error()}")
             continue
+        
+        hora, usuario, valor, operacao = msg.value().decode('utf-8').split('<>')
+        cliente = Cliente.cria_ou_recupera_cliente(usuario)
+        cliente.processa_entrada(valor, operacao)
+
 
         logging.info(f"Mensagem recebida: {msg.value().decode('utf-8')}")
 
+
 if __name__ == "__main__":
-    consome_mensagens()
+    processa_clientes()
 
